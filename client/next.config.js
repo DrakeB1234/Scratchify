aa062773e2b7302b529c924410e3eb03d4ea7d4c
DrakeB1234 <DrakeB1234>
2023-03-30 08:50:47 -0500

    starting directory for use of app directory in next js for supabase database queries

--- client/next.config.js
+++ client/next.config.js
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    })
-
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'wsgtwhbvwnftxaqiogud.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/recipe-images/public/**',
-      },
-    ],
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
 }
+
+module.exports = nextConfig
